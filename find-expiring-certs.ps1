c1e878379edee10daad92dee8a357cc73724128e
Stephen Hellicar <s.hellicar@gmail.com>
2019-02-03 18:58:33 +1100

    Create find-expiring-certs.ps1

---
+++ find-expiring-certs.ps1
@@ -0,0 +1,1 @@
+ Get-ChildItem -Recurse -Path cert: | where { $_.notafter -le (get-date).AddDays(30) -AND $_.notafter -gt (get-date) } | select thumbprint,subject,@{Name='DaysLeft';Expression={($_.notafter-(Get-Date)).Days}}
